f3aafb7126b32b5a3e375f149bcf49a22cc195d6
Pascal Kistler <pascal-kistler@hotmail.com>
2018-05-21 11:17:20 +0530

    Fixed var name

--- dbtest.php
+++ dbtest.php
@@ -3,7 +3,7 @@ $dbhost = getenv("DB_HOST");
 $dbuser = getenv("DB_USER");
 $dbpwd = getenv("DB_PASSWORD");
 $dbname = getenv("DB_DATABASE");
-$dbconn = pg_connect("host=".$dbhost." dbname=".$dbname." user=".$dbuser." password=".$dbpw)
+$dbconn = pg_connect("host=".$dbhost." dbname=".$dbname." user=".$dbuser." password=".$dbpwd)
     or die('Could not connect: ' . pg_last_error());
 echo pg_dbname();
 pg_close($dbconn);
